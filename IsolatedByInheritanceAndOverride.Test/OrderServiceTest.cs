4caf4f356a3411f737e3f7b2892dbd7f34574a0d
91 <jay.hatelove@gmail.com>
2015-02-10 16:59:21 +0800

    Legacy code that depend on File I/O to parse CSV file and depend on web service by HttpClient. And I want to build isolated unit test to verify the logic of "only orders of book can insert to web api".

---
+++ IsolatedByInheritanceAndOverride.Test/OrderServiceTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IsolatedByInheritanceAndOverride.Test
+{
+    /// <summary>
+    /// OrderServiceTest 的摘要描述
+    /// </summary>
+    [TestClass]
+    public class OrderServiceTest
+    {
+        public OrderServiceTest()
+        {
+            //
+            // TODO:  在此加入建構函式的程式碼
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///取得或設定提供目前測試回合
+        ///的相關資訊與功能的測試內容。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 其他測試屬性
+        //
+        // 您可以使用下列其他屬性撰寫您的測試: 
+        //
+        // 執行該類別中第一項測試前，使用 ClassInitialize 執行程式碼
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // 在類別中的所有測試執行後，使用 ClassCleanup 執行程式碼
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // 在執行每一項測試之前，先使用 TestInitialize 執行程式碼 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // 在執行每一項測試之後，使用 TestCleanup 執行程式碼
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Test_SyncBookOrders_3_Orders_Only_2_book_order()
+        {
+            // hard to isolate dependency to unit test
+        }
+    }
+}
---
+++ IsolatedByInheritanceAndOverride/OrderService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+
+namespace IsolatedByInheritanceAndOverride
+{
+    public class OrderService
+    {
+        private string _filePath= @"C:\temp\joey.csv";
+
+        public void SyncBookOrders()
+        {
+            var orders = this.GetOrders();
+
+            // only get orders of book
+            var ordersOfBook = orders.Where(x => x.Type == "Book");
+
+            var bookDao = new BookDao();
+            foreach (var order in ordersOfBook)
+            {
+                bookDao.Insert(order);
+            }
+        }
+
+        private List<Order> GetOrders()
+        {
+            // parse csv file to get orders
+            var result = new List<Order>();
+
+            // directly depend on File I/O
+            using (StreamReader sr = new StreamReader(this._filePath, Encoding.UTF8))
+            {
+                int rowCount = 0;
+
+                while (sr.Peek() > -1)
+                {
+                    rowCount++;
+
+                    var content = sr.ReadLine();
+
+                    // Skip CSV header line
+                    if (rowCount > 1)
+                    {
+                        string[] line = content.Trim().Split(',');
+
+                        result.Add(this.Mapping(line));
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private Order Mapping(string[] line)
+        {
+            var result = new Order
+            {
+                ProductName = line[0],
+                Type = line[1],
+                Price = Convert.ToInt32(line[2]),
+                CustomerName = line[3]
+            };
+
+            return result;
+        }
+    }
+
+    public class Order
+    {
+        public string Type { get; set; }
+
+        public int Price { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string CustomerName { get; set; }
+    }
+
+    public class BookDao
+    {
+        internal void Insert(Order order)
+        {
+            // directly depend on some web service
+            var client = new HttpClient();
+            client.PostAsync<Order>("http://api.joey.io/Order", order, new JsonMediaTypeFormatter());
+
+        }
+    }
+}
